dbb713b9787cd2f32f6983711139922244d3fb93
jslaughter808 <qnqszfzckw@privaterelay.appleid.com>
2026-02-08 21:26:58 -0700

    Fix import order and remove duplicate imports

--- hockey-stats/src/main.jsx
+++ hockey-stats/src/main.jsx
@@ -1,3 +1,6 @@
+import './index.css'  // or your CSS filename
+import App from './App'
+import ReactDOM from 'react-dom/client'
 import React from "react";
 import ReactDOM from "react-dom/client";
 import { BrowserRouter } from "react-router-dom";
